d388f6d61486e209485d844f7d23e158162f82f3
guillem <guillem@disedit.com>
2022-02-26 18:25:27 +0100

    add french translation

--- lang/fr.js
+++ lang/fr.js
@@ -1,17 +1,60 @@
 export default {
   global: {
-    title: '10 props for a Europe of all peoples',
-    EFA: 'European Free Alliance'
+    title: '@:hero.title.0 @:hero.title.1',
+    EFA: 'Alliance Libre Européenne',
+    language: 'Select your language',
+    skip: 'Skip to main content',
+    newwindow: '(opens in a new window)'
+  },
+  meta: {
+    description: '@:hero.subtitle',
+    thumbnail: '/ogimage_en.png',
+    url: ''
   },
   hero: {
-    introduction: ''
+    title: [
+      'Façonner une Europe',
+      'de tous les peuples',
+    ],
+    subtitle: '10 propositions pour la Conférence sur l\'avenir de l\'Europe',
+    introduction: {
+      paragraphs: [
+        `La {0} ({1}) est l'occasion pour les Européens de {2}.
+        L'{3} contribue à ce débat avec {4} pour une UE qui reflète véritablement la diversité culturelle du continent.`,
+        `Nous vous invitons à lire, partager et commenter ces propositions,
+        et à {0} sur la plateforme en ligne officielle #CoFoE ! {1}`
+      ],
+      emphasis: ['faire entendre leur voix', '10 propositions', 'les endosser'],
+      conference: 'Conférence sur l\'avenir de l\'Europe',
+      hashtags: ['#CoFoE', '#TheFutureIsYours'],
+      EFA: '@:global.EFA'
+    },
   },
   proposal: {
-    support: 'Support this proposal',
-    share: 'Share'
+    support: 'Endorse Proposal',
+    share: 'Share',
+    explainer: 'How to endorse a proposal',
+    seeall: 'See all 10 proposals'
+  },
+  explainer: {
+    intro: 'Le processus d\'approbation d\'une proposition est le suivant',
+    list: [
+      "Tout d'abord, créez un compte « EU Login » en vous rendant sur {0} et en cliquant sur le lien pour vous inscrire.",
+      'Suivez les instructions pour créer également un compte sur la plateforme CoFoE',
+      'Accédez à la proposition que vous souhaitez soutenir en utilisant les liens sur cette page.',
+      'Cliquez sur "Approuver" pour ajouter votre vote à la proposition. Vous pouvez également laisser un commentaire si vous le souhaitez.',
+    ],
+    translation: `
+      La plateforme utilise la <strong>traduction automatique</strong>
+      afin que vous puissiez participer dans n'importe quelle langue
+      officielle de l'UE. Vous n'avez <strong>pas besoin d'être basé dans un
+      État membre de l'UE</strong> pour faire une approbation ou un commentaire.`,
+    thanks: 'Merci de participer au débat !'
   },
   footer: {
-    euparl: 'With support...',
-    follow: 'Follow us'
+    euparl: 'With Financial Support of the European Parliament. The information on this website concerns only the author, the European Parliament is not responsible for the use and content of the information.',
+    follow: 'Follow us',
+    contact: 'Contact',
+    privacy: 'Privacy Policy'
   }
 }
