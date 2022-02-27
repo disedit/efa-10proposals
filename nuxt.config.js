b658ce3024600574193671629d58890b6da2ce48
guillem <guillem@disedit.com>
2022-02-27 16:06:47 +0100

    update photos

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,9 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    './plugins/router.js'
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -71,6 +73,7 @@ export default {
     defaultLocale: 'en',
     lazy: true,
     langDir: 'lang/',
+    skipSettingLocaleOnNavigate: true,
   },
 
   eslint: { cache: false },
